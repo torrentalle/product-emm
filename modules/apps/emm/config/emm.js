3038e0b1699cf6b574536634ec7ce4e1dbdf3a23
harshanL <harshan@wso2.com>
2014-09-07 10:52:56 +0530

    EMM server code optimizations.

--- modules/apps/emm/config/emm.js
+++ modules/apps/emm/config/emm.js
@@ -1,24 +1,19 @@
-var config;
-(function () {
-    /* 
-        Will be removing the confusion with the host and ip. 
-        If carbon.xml hostname is available it will use it. 
-        Otherwise it will use the ip. 
-    */
-    config = function () {
-        var log = new Log(),
-            pinch = require('/modules/pinch.min.js').pinch,
-            config = require('/config/config.json'),
+var config = function () {
+    var conf = application.get("PINCH_CONFIG");
+    if (!conf) {
+        var pinch = require('/modules/pinch.min.js').pinch,
             address = require('/modules/address.js');
-
-        pinch(config, /^/, function (path, key, value) {
-            if ((typeof value === 'string') && value.indexOf('%https.ip%') > -1) {
-                return value.replace('%https.ip%', address.getAddress("https"));
-            }else if ((typeof value === 'string') && value.indexOf('%http.ip%') > -1) {
-                return value.replace('%http.ip%', address.getAddress("http"));
-            }
-            return  value;
-        });
-        return config;
-    };
-})();
+        config = require('/config/config.json'),
+            pinch(config, /^/, function (path, key, value) {
+                if ((typeof value === 'string') && value.indexOf('%https.ip%') > -1) {
+                    return value.replace('%https.ip%', address.getAddress("https"));
+                } else if ((typeof value === 'string') && value.indexOf('%http.ip%') > -1) {
+                    return value.replace('%http.ip%', address.getAddress("http"));
+                }
+                return  value;
+            });
+        application.put("PINCH_CONFIG", config);
+        conf = config;
+    }
+    return conf;
+};
