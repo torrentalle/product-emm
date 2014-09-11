709cc515fea4d57a7fbc74ed4e130084fe21bfdc
harshanL <harshan@wso2.com>
2014-09-11 10:35:14 +0530

    Added new copyright license.

--- modules/components/emm-core/src/main/java/org/wso2/mobile/core/EMMDBInitializerException.java
+++ modules/components/emm-core/src/main/java/org/wso2/mobile/core/EMMDBInitializerException.java
@@ -1,6 +1,6 @@
 /*
  * *
- *  *  Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  *  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *  *
  *  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  *  you may not use this file except in compliance with the License.
@@ -17,6 +17,10 @@
  */
 
 package org.wso2.mobile.core;
+
+/**
+ * Custom exception class of EMMDBInitializer
+ */
 
 public class EMMDBInitializerException extends Exception{
 
